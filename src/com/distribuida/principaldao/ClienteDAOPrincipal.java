8d199b3df3d3d5ed94a7450228d2de041b6038c1
DM <DM@LAB-SW3-01.ALUMNOS.LOCAL>
2024-11-27 09:29:43 -0500

    ejecucion de clase principal dao

--- src/com/distribuida/principaldao/ClienteDAOPrincipal.java
+++ src/com/distribuida/principaldao/ClienteDAOPrincipal.java
@@ -15,10 +15,33 @@ public class ClienteDAOPrincipal {
 		
 		ClienteDAO clienteDAO = context.getBean("clienteDAOImpl", ClienteDAO.class);
 		
+		// Insert
+		Cliente cliente = new Cliente(0,"1701234569","Juan","Taipe","Quito","0987654321","jtaipe@correo.com");
+		//clienteDAO.add(cliente);
+
+		// update
+		Cliente cliente2 = new Cliente(39,"1701234666","Juan6","Taipe6","Quito6","09876543216","jtaipe6@correo.com");
+		//clienteDAO.up(cliente2);
+		
+		//findOne
+		try {
+			Cliente cliente3 = clienteDAO.findOne(39);
+			System.out.println(cliente3.toString());	
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		
+		// delete
+		//clienteDAO.del(39);
+						
+		
+		// findAll
 		List<Cliente> clientes = clienteDAO.findAll();
 		
-		for (Cliente cliente : clientes) {
-			System.out.println(cliente.toString());
+		for (Cliente item : clientes) {
+			System.out.println(item.toString());
 		}
 		
 		context.close();
